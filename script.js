f9fbd32ebae77e6f82c39aca53f1736b67edcb3d
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-04 23:49:56 -0700

    Create function to play one round

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"];
+  let choices = ["rock", "paper", "scissors"];
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase();
+  let computer = computerSelection;
+
+  if (player === "rock" && computer === "paper") {
+    return "You lost! Computer wins.";
+  } else if (player === "rock" && computer === "scissors") {
+    return "You won! Congratulations!";
+  } else if (player === "paper" && computer === "scissors") {
+    return "You lost! Computer wins.";
+  } else if (player === "paper" && computer === "rock") {
+    return "You won! Congratulations!";
+  } else if (player === "scissors" && computer === "rock") {
+    return "You lost! Computer wins.";
+  } else if (player === "scissors" && computer === "paper") {
+    return "You won! Congratulations!";
+  } else {
+    return "It's a tie!";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
